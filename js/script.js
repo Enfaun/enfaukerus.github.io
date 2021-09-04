aa354ea4a3b7d677be2ae0993c45cdc9cb1552e8
Enfau Kerus <kerus@enfa.uk>
2021-09-04 09:04:37 +0000

    add minimize button, change content

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 window.addEventListener(
   "load",
   function(){
+    document.querySelector("#minimized").style = "";
     document.querySelector("#close")
       .addEventListener(
         "click", 
         function(){ document.querySelector("#window").hidden = true; }
-      )
+      );
+    document.querySelector("#closeMinimized")
+      .addEventListener(
+        "click", 
+        function(){ document.querySelector("#minimized").hidden = true; }
+      );
+    document.querySelector("#minimize")
+      .addEventListener(
+        "click", 
+        function(){ document.querySelector("#window").hidden = true; document.querySelector("#minimized").hidden = false; }
+      );
+    document.querySelector("#restore")
+      .addEventListener(
+        "click", 
+        function(){ document.querySelector("#window").hidden = false; document.querySelector("#minimized").hidden = true; }
+      );
   }
 )
